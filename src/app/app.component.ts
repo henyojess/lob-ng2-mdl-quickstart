bcbcc3853b0cb4b8a6b75fc999000878aabe63ad
henyojess <jessie@henyo.com>
2016-11-25 16:46:41 +0800

    added angilar2-mdl dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'LOB NG2-MDL Frontend Quickstart!';
 }
